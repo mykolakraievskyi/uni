c1e88f51c17aaa42249c4862539b21328a32f0cb
mykolakraievskyi <m.kraievskyi@gmail.com>
2024-10-07 15:51:20 +0300

    added random data generation

--- software design/TaxiDbFirst/TaxiDbFirst/MainWindowViewModel.cs
+++ software design/TaxiDbFirst/TaxiDbFirst/MainWindowViewModel.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bogus;
 using TaxiDbFirst.Model.TaxiDbContext;
 
 namespace TaxiDbFirst
@@ -119,7 +121,7 @@ namespace TaxiDbFirst
             }
         }
 
-        private TaxiContext _context;
+        private readonly TaxiContext _context;
 
         public MainWindowViewModel()
         {
@@ -138,109 +140,167 @@ namespace TaxiDbFirst
 
         private void AddRandomData()
         {
-            var random = new Random();
+            var faker = new Faker();
+            var sqlScripts = new List<string>();
 
-            for (int i = 0; i < 5; i++)
+            for (var i = 0; i < 100; i++)
             {
                 var newCustomer = new Customer
                 {
-                    Number = random.Next(1006, 9999), 
-                    Firstname = GenerateRandomFirstName(),
-                    Lastname = GenerateRandomLastName(),
-                    DateOfBirth = GenerateRandomDateOfBirth(),
+                    Number = faker.Random.Int(1000, 9999),
+                    Firstname = faker.Name.FirstName(),
+                    Lastname = faker.Name.LastName(),
+                    DateOfBirth = DateOnly.FromDateTime(faker.Date.Past(50, DateTime.Now.AddYears(-18))),
                     PhoneNumber = GenerateUniquePhoneNumber(),
                     Email = GenerateUniqueEmail(),
-                    ClientFrom =
-                        DateOnly.FromDateTime(
-                            DateTime.Now.AddDays(-random.Next(100, 1000))) 
+                    ClientFrom = DateOnly.FromDateTime(faker.Date.Past(10))
                 };
 
                 _context.Customers.Add(newCustomer);
+                sqlScripts.Add(CreateInsertScript(newCustomer));
             }
 
-            for (int i = 0; i < 5; i++)
+            for (var i = 0; i < 100; i++)
             {
                 var newDriver = new Driver
                 {
-                    Number = random.Next(2006, 9999), 
-                    Firstname = GenerateRandomFirstName(),
-                    Lastname = GenerateRandomLastName(),
-                    DateOfBirth = GenerateRandomDateOfBirth(),
+                    Number = faker.Random.Int(2000, 9999),
+                    Firstname = faker.Name.FirstName(),
+                    Lastname = faker.Name.LastName(),
+                    DateOfBirth = DateOnly.FromDateTime(faker.Date.Past(50, DateTime.Now.AddYears(-18))),
                     PhoneNumber = GenerateUniquePhoneNumber(),
                     Email = GenerateUniqueEmail(),
-                    Rating = Math.Round((float)(random.NextDouble() * (5 - 3.5) + 3.5), 2),
-                    StartedWorkingOn =
-                        DateOnly.FromDateTime(DateTime.Now.AddYears(-random.Next(1, 5)))
+                    Rating = faker.Random.Float(3.5f, 5),
+                    StartedWorkingOn = DateOnly.FromDateTime(faker.Date.Past(5))
                 };
 
                 _context.Drivers.Add(newDriver);
+                sqlScripts.Add(CreateInsertScript(newDriver));
+            }
+
+            for (var i = 0; i < 100; i++)
+            {
+                var newVehicle = new Vehicle
+                {
+                    Number = $"VH{i + 1:D3}",
+                    LicensePlate = faker.Vehicle.Vin(),
+                    Model = faker.Vehicle.Model(),
+                    Manufacturer = faker.Vehicle.Manufacturer(),
+                    Year = faker.Date.Past(10).Year,
+                    Seats = faker.Random.Int(2, 7),
+                    CategoryId = 2
+                };
+
+                _context.Vehicles.Add(newVehicle);
+                sqlScripts.Add(CreateInsertScript(newVehicle));
+            }
+            for (var i = 0; i < 100; i++)
+            {
+                var newPoint = new Point
+                {
+                    Longitude = faker.Address.Longitude(),
+                    Latitude = faker.Address.Latitude(),
+                    City = faker.Address.City(),
+                    Street = faker.Address.StreetName(),
+                    Number = faker.Address.BuildingNumber(),
+                    Name = faker.Address.SecondaryAddress()
+                };
+
+                _context.Points.Add(newPoint);
+                sqlScripts.Add(CreateInsertScript(newPoint));
+            }
+            _context.SaveChanges();
+
+            for (var i = 0; i < 300; i++)
+            {
+                var vehicle = _context.Vehicles.Select(x => x.Id).ElementAt(faker.Random.Int(0, _context.Vehicles.Count() - 1));
+                var driver = _context.Drivers.Select(x => x.Id).ElementAt(faker.Random.Int(0, _context.Drivers.Count() - 1));
+                var customer = _context.Customers.Select(x => x.Id).ElementAt(faker.Random.Int(0, _context.Customers.Count() - 1));
+                var startPoint = _context.Points.Select(x => x.Id).ElementAt(faker.Random.Int(0, _context.Points.Count() - 1));
+                var endPoint = _context.Points.Select(x => x.Id).ElementAt(faker.Random.Int(0, _context.Points.Count() - 1));
+                var newTrip = new Trip
+                {
+                    Number = $"TRP{i + 1:D3}",
+                    VehicleId = vehicle, 
+                    DriverId = driver, 
+                    CustomerId = customer, 
+                    StatusId = faker.Random.Int(1, 5), 
+                    PickUpTime = DateOnly.FromDateTime(faker.Date.Past(1)),
+                    StartPointId = startPoint,
+                    EndPointId = endPoint,
+                    Cost = faker.Random.Float(50, 500),
+                    Rating = faker.Random.Int(1, 5)
+                };
+
+                _context.Trips.Add(newTrip);
+                sqlScripts.Add(CreateInsertScript(newTrip));
             }
 
             _context.SaveChanges();
+            SaveSqlScriptsToFile(sqlScripts);
 
-            Categories = new ObservableCollection<Category>(_context.Categories.ToList());
             Customers = new ObservableCollection<Customer>(_context.Customers.ToList());
             Drivers = new ObservableCollection<Driver>(_context.Drivers.ToList());
-            DriverQualifications =
-                new ObservableCollection<DriverQualification>(_context.DriverQualifications.ToList());
-            NewDrivers = new ObservableCollection<NewDriver>(_context.NewDrivers.ToList());
+            Vehicles = new ObservableCollection<Vehicle>(_context.Vehicles.ToList());
             Points = new ObservableCollection<Point>(_context.Points.ToList());
             Trips = new ObservableCollection<Trip>(_context.Trips.ToList());
-            TripStatuses = new ObservableCollection<TripStatus>(_context.TripStatuses.ToList());
-            Vehicles = new ObservableCollection<Vehicle>(_context.Vehicles.ToList());
-        }
-
-        private string GenerateRandomFirstName()
-        {
-            var firstNames = new List<string>
-                { "John", "Jane", "Alice", "Robert", "Emily", "Michael", "Olga", "Mykola", "Oksana", "Yaroslav" };
-            return firstNames[new Random().Next(firstNames.Count)];
-        }
-
-        private string GenerateRandomLastName()
-        {
-            var lastNames = new List<string>
-            {
-                "Doe", "Smith", "Johnson", "Williams", "Davis", "Brown", "Vorobets", "Kraievskyi", "Shevchenko",
-                "Kovalenko"
-            };
-            return lastNames[new Random().Next(lastNames.Count)];
-        }
-
-        private DateOnly GenerateRandomDateOfBirth()
-        {
-            var random = new Random();
-            int year = random.Next(1950, 2005); 
-            int month = random.Next(1, 13);
-            int day = random.Next(1, 29); 
-            return new DateOnly(year, month, day);
         }
 
         private string GenerateUniquePhoneNumber()
         {
-            var phoneNumber = $"097{new Random().Next(1000000, 9999999)}";
-
-            while (_context.Customers.Any(c => c.PhoneNumber == phoneNumber) ||
-                   _context.Drivers.Any(d => d.PhoneNumber == phoneNumber))
+            var faker = new Faker();
+            string phoneNumber;
+            do
             {
-                phoneNumber = $"097{new Random().Next(1000000, 9999999)}";
-            }
+                phoneNumber = faker.Random.String2(10, "0123456789"); 
+            } while (_context.Customers.Any(c => c.PhoneNumber == phoneNumber) ||
+                     _context.Drivers.Any(d => d.PhoneNumber == phoneNumber));
 
             return phoneNumber;
         }
 
         private string GenerateUniqueEmail()
         {
-            var random = new Random();
-            var email = $"user{random.Next(1000, 9999)}@example.com";
-
-            while (_context.Customers.Any(c => c.Email == email) ||
-                   _context.Drivers.Any(d => d.Email == email))
+            var faker = new Faker();
+            string email;
+            do
             {
-                email = $"user{random.Next(1000, 9999)}@example.com";
-            }
+                email = faker.Internet.Email();
+            } while (_context.Customers.Any(c => c.Email == email) ||
+                     _context.Drivers.Any(d => d.Email == email));
 
             return email;
+        }
+
+        private static string CreateInsertScript(object entity)
+        {
+            return entity switch
+            {
+                Customer customer =>
+                    $"INSERT INTO Customers (Number, Firstname, Lastname, DateOfBirth, PhoneNumber, Email, ClientFrom) " +
+                    $"VALUES ({customer.Number}, '{customer.Firstname}', '{customer.Lastname}', '{customer.DateOfBirth}', " +
+                    $"'{customer.PhoneNumber}', '{customer.Email}', '{customer.ClientFrom}');",
+                Driver driver =>
+                    $"INSERT INTO Drivers (Number, Firstname, Lastname, DateOfBirth, PhoneNumber, Email, Rating, StartedWorkingOn) " +
+                    $"VALUES ({driver.Number}, '{driver.Firstname}', '{driver.Lastname}', '{driver.DateOfBirth}', " +
+                    $"'{driver.PhoneNumber}', '{driver.Email}', {driver.Rating}, '{driver.StartedWorkingOn}');",
+                Vehicle vehicle =>
+                    $"INSERT INTO Vehicles (Number, LicensePlate, Model, Manufacturer, Year, Seats, CategoryId) " +
+                    $"VALUES ('{vehicle.Number}', '{vehicle.LicensePlate}', '{vehicle.Model}', '{vehicle.Manufacturer}', " +
+                    $"{vehicle.Year}, {vehicle.Seats}, {vehicle.CategoryId});",
+                Point point => $"INSERT INTO Points (Longitude, Latitude, City, Street, Number, Name) " +
+                               $"VALUES ({point.Longitude}, {point.Latitude}, '{point.City}', '{point.Street}', '{point.Number}', '{point.Name}');",
+                Trip trip =>
+                    $"INSERT INTO Trips (Number, VehicleId, DriverId, CustomerId, StatusId, PickUpTime, StartPointId, EndPointId, Cost, Rating) " +
+                    $"VALUES ('{trip.Number}', {trip.VehicleId}, {trip.DriverId}, {trip.CustomerId}, {trip.StatusId}, " +
+                    $"'{trip.PickUpTime}', {trip.StartPointId}, {trip.EndPointId}, {trip.Cost}, {trip.Rating});",
+                _ => string.Empty
+            };
+        }
+
+        private static void SaveSqlScriptsToFile(List<string> sqlScripts)
+        {
+            File.WriteAllLines("GeneratedData.sql", sqlScripts);
         }
     }
 }
