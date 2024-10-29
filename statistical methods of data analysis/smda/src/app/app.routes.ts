3485405e10fc9a3f47a6133351a32279f5bbc6b2
mykolakraievskyi <m.kraievskyi@gmail.com>
2024-10-29 21:26:33 +0200

    deleted smda

--- statistical methods of data analysis/smda/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { StatisticsTabComponent } from './components/statistics-tab/statistics-tab.component';
-
-export const routes: Routes = [
-  { path: 'task1', component: StatisticsTabComponent },
-  //{ path: 'task2', component: PlaceholderComponent }, // Placeholder for other tasks
-  { path: '', redirectTo: '/task1', pathMatch: 'full' },
-];
